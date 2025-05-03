5c24b989ad6aeea8f2c658c6ae81a880731a7f8d
Shubham-resp <sj.innovations67@gmail.com>
2025-05-03 22:11:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ searchBtn.addEventListener("click", async () => {
     return;
   }
 
-  const API_KEY = "AIzaSyAXSNrsnp-ikNLvGqH0O4aYxLDPdbbtiPE"; // Replace with your API key
+  const API_KEY = "YOUTUBE_API_KEY"; // Replace with your API key
 
   try {
     // Search channel by name
